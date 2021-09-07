2a364d63d5b703fe67e49a6cc94ac1cfc95146d4
Liyang <liyang6457@cvte.com>
2021-09-07 17:03:11 +0800

    test11

--- scripts/windows-mingw-publish.ps1
+++ scripts/windows-mingw-publish.ps1
@@ -17,9 +17,11 @@ function Main() {
 
     New-Item -ItemType Directory $archiveName
     # 拷贝exe
-    Copy-Item bin\release\$targetName $archiveName\
+    Copy-Item bin\$targetName $archiveName\
+    Copy-Item bin\** $archiveName\ -Recurse
     # 拷贝依赖
-    windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
+    windeployqt --plugindir $archiveName\plugins --compiler-runtime $archiveName\$targetName
+    windeployqt --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\LBL_CommunicatEngine.dll
     # 删除不必要的文件
     $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
     Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
